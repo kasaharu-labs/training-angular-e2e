17df9b6db59d103ead470670a548ca49448e0182
Wataru.Kasahara <kasahara1019@gmail.com>
2019-09-02 09:48:26 +0900

    top page へのアクセスはどのテストでも実施するので beforeEach に集約

--- cypress/integration/first-test.spec.ts
+++ cypress/integration/first-test.spec.ts
@@ -2,14 +2,14 @@ describe('First test', () => {
   let baseUrl;
   beforeEach(() => {
     baseUrl = 'http://localhost:4200';
+    cy.visit(baseUrl);
+    cy.wait(500);
   });
 
   describe('should visit top page', () => {
     it('/dashboard に遷移する', () => {
       const redirectUrl = `${baseUrl}/dashboard`;
 
-      cy.visit(baseUrl);
-      cy.wait(500);
       cy.url().should('eq', redirectUrl);
       cy.screenshot();
     });
@@ -18,9 +18,6 @@ describe('First test', () => {
   describe('click link in nav', () => {
     it('Dashboard をクリックすると /dashboard に遷移する', () => {
       const redirectUrl = `${baseUrl}/dashboard`;
-
-      cy.visit(baseUrl);
-      cy.wait(500);
 
       cy.get('nav')
         .find('a')
@@ -33,9 +30,6 @@ describe('First test', () => {
 
     it('Heroes をクリックすると /heroes に遷移する', () => {
       const redirectUrl = `${baseUrl}/heroes`;
-
-      cy.visit(baseUrl);
-      cy.wait(500);
 
       cy.get('nav')
         .find('a')
