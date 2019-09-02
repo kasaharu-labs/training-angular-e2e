b5f1d20e13c2a020a9996c8dfeb7cf63112b9666
Wataru.Kasahara <kasahara1019@gmail.com>
2019-09-02 09:46:47 +0900

    top page の nav 内にある link をクリックしたときのテストを追加

--- cypress/integration/first-test.spec.ts
+++ cypress/integration/first-test.spec.ts
@@ -14,4 +14,36 @@ describe('First test', () => {
       cy.screenshot();
     });
   });
+
+  describe('click link in nav', () => {
+    it('Dashboard をクリックすると /dashboard に遷移する', () => {
+      const redirectUrl = `${baseUrl}/dashboard`;
+
+      cy.visit(baseUrl);
+      cy.wait(500);
+
+      cy.get('nav')
+        .find('a')
+        .contains('Dashboard')
+        .click();
+      cy.wait(500);
+      cy.url().should('eq', redirectUrl);
+      cy.screenshot();
+    });
+
+    it('Heroes をクリックすると /heroes に遷移する', () => {
+      const redirectUrl = `${baseUrl}/heroes`;
+
+      cy.visit(baseUrl);
+      cy.wait(500);
+
+      cy.get('nav')
+        .find('a')
+        .contains('Heroes')
+        .click();
+      cy.wait(500);
+      cy.url().should('eq', redirectUrl);
+      cy.screenshot();
+    });
+  });
 });
