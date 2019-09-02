fbe79d77e40920e778a88c655c38154180a5439c
Wataru.Kasahara <kasahara1019@gmail.com>
2019-09-02 09:42:57 +0900

    top page にアクセスしたときのテストを追加

--- cypress/integration/first-test.spec.ts
+++ cypress/integration/first-test.spec.ts
@@ -1,7 +1,17 @@
 describe('First test', () => {
-  it('should visit login page', () => {
-    cy.visit('http://localhost:4200');
-    cy.wait(500);
-    cy.screenshot();
+  let baseUrl;
+  beforeEach(() => {
+    baseUrl = 'http://localhost:4200';
+  });
+
+  describe('should visit top page', () => {
+    it('/dashboard に遷移する', () => {
+      const redirectUrl = `${baseUrl}/dashboard`;
+
+      cy.visit(baseUrl);
+      cy.wait(500);
+      cy.url().should('eq', redirectUrl);
+      cy.screenshot();
+    });
   });
 });
