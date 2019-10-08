19dc4817a67a7a69689e14d1567047d4bb57e1a0
Wataru.Kasahara <kasahara1019@gmail.com>
2019-10-09 01:49:18 +0900

    webpack.partial.js の設定変更

--- webpack.partial.js
+++ webpack.partial.js
@@ -1,5 +1,9 @@
 const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
 
 module.exports = {
+  output: {
+    path: __dirname + '/dist/training-angular-e2e',
+    filename: '[name].js',
+  },
   plugins: [new BundleAnalyzerPlugin({ token: process.env.BUNDLE_ANALYZER_TOKEN })],
 }
